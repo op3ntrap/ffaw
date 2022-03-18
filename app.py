51c9e8783002d0aeffeddb6b71e7f59720810f58
Risan Raja <op3ntrap@gmail.com>
2022-03-19 03:18:50 +0530

    Sample code to test working of the API endpoint within the application context.

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from . import app
-# import requests
+import requests
 # from models import User
 
-#
-# @app.route('/')
-# def home():
-# 	# eew = User.query.all()
-# 	q = requests.get('http://localhost:5000/api/user/1')
-# 	return q.text
+
+@app.route('/')
+def home():
+	# eew = User.query.all()
+	q = requests.get('http://localhost:5000/api/tracker/1')
+	return q.text
 
 
 if __name__ == '__main__':
